115b392abc0ff699bdff9196db2ade3a986b700d
Kaj-Michael Lang <milang@tal.org>
2023-04-16 23:19:11 +0300

    More media info reading

--- src/filereader.cpp
+++ src/filereader.cpp
@@ -24,10 +24,41 @@ int FileReader::getMetaData(const QString file)
     int ret;
 
     if ((ret = avformat_open_input(&fmt_ctx, file.toLocal8Bit().data(), NULL, NULL)))
-        return ret;
+        return ret;        
+
+    qDebug() << "format" << fmt_ctx->iformat->name;
+    qDebug() << "duration" << fmt_ctx->duration;
+    qDebug() << "bitrate" << fmt_ctx->bit_rate;
+    qDebug() << "streams" << fmt_ctx->nb_streams;        
 
     while ((tag = av_dict_get(fmt_ctx->metadata, "", tag, AV_DICT_IGNORE_SUFFIX)))
         qDebug() << tag->key << tag->value;
+
+    if (avformat_find_stream_info(fmt_ctx,  NULL) < 0) {
+        qDebug() << "avformat_find_stream_info fail";
+        return -1;
+    }
+
+    for (int i = 0; i < fmt_ctx->nb_streams; i++) {
+        qDebug() << "stream info for" << i;
+
+        const auto *stream = fmt_ctx->streams[i];
+
+        qDebug() << "start_time" << stream->start_time;
+        qDebug() << "duration" << stream->duration;
+        qDebug() << "codec" << stream->codecpar->codec_type;
+
+        const auto &factor = stream->time_base;
+
+        if (stream->duration > 0 && factor.num > 0 && factor.den > 0) {
+            auto dur=qint64(1000000) * stream->duration * factor.num / factor.den;
+            qDebug() << "duration adjusted" << dur/1000.0;
+        } else if (stream->duration < 0) {
+            const auto duration = av_dict_get(stream->metadata, "DURATION", nullptr, 0);
+            qDebug() << "duration from metadata" << duration;
+        }
+
+    }
 
     avformat_free_context(fmt_ctx);
 #endif
